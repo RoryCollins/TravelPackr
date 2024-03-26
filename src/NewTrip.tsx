879034081e99942d65e4b108f7269817b57e9f22
Rory Collins <rory.collins@hotmail.co.uk>
2024-03-26 09:31:47 +0000

    render templates from list

--- src/NewTrip.tsx
+++ src/NewTrip.tsx
@@ -1,31 +1,36 @@
 import { Box, Button, Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 
-
 function SubContents({ value }: { value: boolean }) {
+    const templates = [
+        {
+            name: "New Template...",
+            image: "/cards/background.jpg"
+        },
+        {
+            name: "Ferry to Ireland",
+            image: "/cards/ireland.jpg"
+        }];
+
+    function clickCard(text: string) {
+        alert(text);
+    }
+
     if (!value) return null;
     return (
-        <Box sx={{display: 'flex', flexDirection: 'row', alignItems: "center"}}>
-            <Card sx={{ maxWidth: 345 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: "center" }}>
+            {templates.map(template =>
+                <Card sx={{ maxWidth: 345, margin: 1 }} onClick={() => clickCard(template.name)}>
                     <CardMedia
                         sx={{ height: 140 }}
-                        image="/cards/background.jpg"
+                        image={template.image}
                         title="holiday"
                     />
-                <CardContent>
-                    <Typography variant="h5">New Template</Typography>
-                </CardContent>
-            </Card>
-            <Card sx={{ maxWidth: 345, margin: 2 }}>
-                <CardMedia
-                    sx={{ height: 140 }}
-                    image="/cards/ireland.jpg"
-                    title="holiday"
-                />
-                <CardContent>
-                    <Typography variant="h5">Ferry to Ireland</Typography>
-                </CardContent>
-            </Card>
+                    <CardContent>
+                        <Typography variant="h5">{template.name}</Typography>
+                    </CardContent>
+                </Card>
+            )}
         </Box>
 
     );
